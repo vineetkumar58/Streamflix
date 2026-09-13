e468877b34cd93869087a4661908d3326afb6c9c
vineetkumar58 <vineet123koli@gmail.com>
2026-09-13 20:52:36 +0530

    authentication ..login..register addition

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Config:
+
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "streamflix-development-secret"
+    )
+
+    DB_HOST = os.getenv(
+        "DB_HOST",
+        "localhost"
+    )
+
+    DB_PORT = int(
+        os.getenv(
+            "DB_PORT",
+            "3306"
+        )
+    )
+
+    DB_USER = os.getenv(
+        "DB_USER",
+        "streamflix"
+    )
+
+    DB_PASSWORD = os.getenv(
+        "DB_PASSWORD",
+        "streamflix_password"
+    )
+
+    DB_NAME = os.getenv(
+        "DB_NAME",
+        "streamflix"
+    )
--- database/db.py
+++ database/db.py
@@ -1,16 +1,20 @@
-import os
 import mysql.connector
-from dotenv import load_dotenv
 
-load_dotenv()
+from config import Config
 
 
 def get_connection():
 
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", "3306")),
-        user=os.getenv("DB_USER", "streamflix"),
-        password=os.getenv("DB_PASSWORD", "streamflix_password"),
-        database=os.getenv("DB_NAME", "streamflix")
+
+        host=Config.DB_HOST,
+
+        port=Config.DB_PORT,
+
+        user=Config.DB_USER,
+
+        password=Config.DB_PASSWORD,
+
+        database=Config.DB_NAME
+
     )
